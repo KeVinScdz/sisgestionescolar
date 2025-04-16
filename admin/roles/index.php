98cf7f53b299c8fff0dea2e2294faff811c35d9d
D4VID12 <davidnj865@gmail.com>
2025-04-16 02:46:10 -0500

    dormulario y asinacion roles

--- admin/roles/index.php
+++ admin/roles/index.php
@@ -2,7 +2,7 @@
 include ('../../app/config.php');
 include ('../../admin/layout/parte1.php');
 include ('../../app/controllers/roles/listado_de_roles.php');
-
+include ('../../app/controllers/roles/listado_de_permisos.php');
 ?>
 
 <!-- Content Wrapper. Contains page content -->
@@ -15,7 +15,6 @@ include ('../../app/controllers/roles/listado_de_roles.php');
             </div>
             <br>
             <div class="row">
-
                 <div class="col-md-8">
                     <div class="card card-outline card-primary">
                         <div class="card-header">
@@ -27,55 +26,100 @@ include ('../../app/controllers/roles/listado_de_roles.php');
                         <div class="card-body">
                             <table id="example1" class="table table-striped table-bordered table-hover table-sm">
                                 <thead>
-                                <tr>
-                                    <th><center>Nro</center></th>
-                                    <th><center>Nombre del rol</center></th>
-                                    <th><center>Acciones</center></th>
-                                </tr>
+                                    <tr>
+                                        <th><center>Nro</center></th>
+                                        <th><center>Nombre del rol</center></th>
+                                        <th><center>Acciones</center></th>
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                <?php
-                                $contador_rol = 0;
-                                foreach ($roles as $role){
-                                    $id_rol = $role['id_rol'];
-                                    $contador_rol = $contador_rol +1; ?>
-                                    <tr>
-                                        <td style="text-align: center"><?=$contador_rol;?></td>
-                                        <td><?=$role['nombre_rol'];?></td>
-                                        <td style="text-align: center">
-                                            <div class="btn-group" role="group" aria-label="Basic example">
-                                                <a href="show.php?id=<?=$id_rol;?>" type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></a>
-                                                <a href="edit.php?id=<?=$id_rol;?>" type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
-                                                <form action="<?=APP_URL;?>/app/controllers/roles/delete.php" onclick="preguntar<?=$id_rol;?>(event)" method="post" id="miFormulario<?=$id_rol;?>">
-                                                    <input type="text" name="id_rol" value="<?=$id_rol;?>" hidden>
-                                                    <button type="submit" class="btn btn-danger btn-sm" style="border-radius: 0px 5px 5px 0px"><i class="bi bi-trash"></i></button>
-                                                </form>
-                                                <script>
-                                                    function preguntar<?=$id_rol;?>(event) {
-                                                        event.preventDefault();
-                                                        Swal.fire({
-                                                            title: 'Eliminar registro',
-                                                            text: '¿Desea eliminar este registro?',
-                                                            icon: 'question',
-                                                            showDenyButton: true,
-                                                            confirmButtonText: 'Eliminar',
-                                                            confirmButtonColor: '#a5161d',
-                                                            denyButtonColor: '#270a0a',
-                                                            denyButtonText: 'Cancelar',
-                                                        }).then((result) => {
-                                                            if (result.isConfirmed) {
-                                                                var form = $('#miFormulario<?=$id_rol;?>');
-                                                                form.submit();
-                                                            }
-                                                        });
-                                                    }
-                                                </script>
-                                            </div>
-                                        </td>
-                                    </tr>
                                     <?php
-                                }
-                                ?>
+                                    $contador_rol = 0;
+                                    foreach ($roles as $role){
+                                        $id_rol = $role['id_rol'];
+                                        $contador_rol = $contador_rol +1; ?>
+                                        <tr>
+                                            <td style="text-align: center"><?=$contador_rol;?></td>
+                                            <td><?=$role['nombre_rol'];?></td>
+                                            <td style="text-align: center">
+                                                <div class="btn-group" role="group" aria-label="Basic example">
+                                                    <!-- Button trigger modal -->
+                                                    <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#modal_signacion<?=$id_rol?>">
+                                                        <i class="bi bi-check-circle"></i>
+                                                    </button>
+
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="modal_signacion<?=$id_rol?>" tabindex="-1" aria-labelledby="exampleModalLabel<?=$id_rol?>" aria-hidden="true">
+                                                        <div class="modal-dialog modal-lg">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header" style="background-color: #dbcd59;">
+                                                                    <h5 class="modal-title" id="exampleModalLabel<?=$id_rol?>">Asignación de roles</h5>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar">
+                                                                        <span aria-hidden="true">&times;</span> 
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <div class="row">
+                                                                        <div class="col-md-3">
+                                                                            <label>Rol: <?=$role['nombre_rol'];?></label>  
+                                                                        </div>
+                                                                        <div class="col-md-6">
+                                                                            <select name="" id="" class="form-control">
+                                                                                <?php
+                                                                                foreach ($permisos as $permiso){
+                                                                                    $id_permiso = $permiso['id_permiso']; ?>
+                                                                                    <option value="<?=$id_permiso?>"><?=$permiso['nombre_url'];?></option>
+                                                                                    <?php
+                                                                                }
+                                                                                ?>
+                                                                            </select>
+                                                                        </div>
+                                                                        <div class="col-md-3">
+                                                                            <button type="submit" class="btn btn-primary mb-2">Asignar</button>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="row">
+                                                                        <table>
+                                                                            
+                                                                        </table>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <a href="show.php?id=<?=$id_rol;?>" type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></a>
+                                                    <a href="edit.php?id=<?=$id_rol;?>" type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></a>
+                                                    <form action="<?=APP_URL;?>/app/controllers/roles/delete.php" onclick="preguntar<?=$id_rol;?>(event)" method="post" id="miFormulario<?=$id_rol;?>">
+                                                        <input type="text" name="id_rol" value="<?=$id_rol;?>" hidden>
+                                                        <button type="submit" class="btn btn-danger btn-sm" style="border-radius: 0px 5px 5px 0px"><i class="bi bi-trash"></i></button>
+                                                    </form>
+                                                    <script>
+                                                        function preguntar<?=$id_rol;?>(event) {
+                                                            event.preventDefault();
+                                                            Swal.fire({
+                                                                title: 'Eliminar registro',
+                                                                text: '¿Desea eliminar este registro?',
+                                                                icon: 'question',
+                                                                showDenyButton: true,
+                                                                confirmButtonText: 'Eliminar',
+                                                                confirmButtonColor: '#a5161d',
+                                                                denyButtonColor: '#270a0a',
+                                                                denyButtonText: 'Cancelar',
+                                                            }).then((result) => {
+                                                                if (result.isConfirmed) {
+                                                                    var form = $('#miFormulario<?=$id_rol;?>');
+                                                                    form.submit();
+                                                                }
+                                                            });
+                                                        }
+                                                    </script>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
@@ -90,12 +134,11 @@ include ('../../app/controllers/roles/listado_de_roles.php');
 <!-- /.content-wrapper -->
 
 <?php
-
 include ('../../admin/layout/parte2.php');
 include ('../../layout/mensajes.php');
-
 ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>
     $(function () {
         $("#example1").DataTable({
@@ -129,21 +172,21 @@ include ('../../layout/mensajes.php');
                     extend: 'copy',
                 }, {
                     extend: 'pdf'
-                },{
+                }, {
                     extend: 'csv'
-                },{
+                }, {
                     extend: 'excel'
-                },{
+                }, {
                     text: 'Imprimir',
                     extend: 'print'
                 }
                 ]
             },
-                {
-                    extend: 'colvis',
-                    text: 'Visor de columnas',
-                    collectionLayout: 'fixed three-column'
-                }
+            {
+                extend: 'colvis',
+                text: 'Visor de columnas',
+                collectionLayout: 'fixed three-column'
+            }
             ],
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     });
