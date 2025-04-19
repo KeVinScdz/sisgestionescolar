b15d4c592c51d11f39fecd794ded3b395b0a6bd0
D4VID12 <davidnj865@gmail.com>
2025-04-18 21:33:57 -0500

    revisar

--- admin/layout/parte1.php
+++ admin/layout/parte1.php
@@ -466,5 +466,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </nav>
             <!-- /.sidebar-menu -->
         </div>
-        <!-- /.sidebar -->
+        <!-- /.sidebar completo -->
     </aside>
+
