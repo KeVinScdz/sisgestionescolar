bd5571c72d8d1cea86a066b3013ec304abc17e06
4ngeloRC <ramosangelo621@gmail.com>
2025-04-21 18:26:27 -0500

    ajustes eliminando registros p2

--- app/controllers/roles/delete_permiso.php
+++ app/controllers/roles/delete_permiso.php
@@ -10,14 +10,21 @@ $sentencia = $pdo->prepare("DELETE FROM permisos where id_permiso=:id_permiso");
 $sentencia->bindParam('id_permiso',$id_permiso);
 
 
-if($sentencia->execute()){
+try {
+    if($sentencia->execute()){
+        session_start();
+        $_SESSION['mensaje'] = "Se elimino el permiso de la manera correcta en la base de datos";
+        $_SESSION['icono'] = "success";
+        header('Location:'.APP_URL."/admin/roles/permisos.php");
+    }else{
+        session_start();
+        $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, comuniquese con el administrador";
+        $_SESSION['icono'] = "error";
+        header('Location:'.APP_URL."/admin/roles/permisos.php");
+    }
+} catch (Exception $exception) {
     session_start();
-    $_SESSION['mensaje'] = "Se elimino el permiso de la manera correcta en la base de datos";
-    $_SESSION['icono'] = "success";
-    header('Location:'.APP_URL."/admin/roles/permisos.php");
-}else{
-    session_start();
-    $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, comuniquese con el administrador";
-    $_SESSION['icono'] = "error";
-    header('Location:'.APP_URL."/admin/roles/permisos.php");
+        $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, porque este registro ya esta en otras tablas";
+        $_SESSION['icono'] = "error";
+        header('Location:'.APP_URL."/admin/roles/permisos.php");
 }
