d5b08c487d1ca8a4887001acf2c1203cd3c8e0de
4ngeloRC <ramosangelo621@gmail.com>
2025-04-21 17:56:16 -0500

    ajustes eliminando registros p1

--- app/controllers/configuraciones/institucion/delete.php
+++ app/controllers/configuraciones/institucion/delete.php
@@ -2,23 +2,29 @@
 
 include ('../../../../app/config.php');
 
-$id_config_institucion = $_POST['id_config_institucion'];
+$id_gestion = $_POST['id_gestion'];
 
 
-$sentencia = $pdo->prepare("DELETE FROM configuracion_instituciones where id_config_institucion=:id_config_institucion ");
+$sentencia = $pdo->prepare("DELETE FROM gestiones where id_gestion=:id_gestion ");
 
-$sentencia->bindParam('id_config_institucion',$id_config_institucion);
+$sentencia->bindParam('id_gestion',$id_gestion);
 
-
-if($sentencia->execute()){
+try{
+    if($sentencia->execute()){
+        session_start();
+        $_SESSION['mensaje'] = "Se elimino los datos de la institución de la manera correcta en la base de datos";
+        $_SESSION['icono'] = "success";
+        header('Location:'.APP_URL."/admin/configuraciones/institucion");
+    }else{
+        session_start();
+        $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, comuniquese con el administrador";
+        $_SESSION['icono'] = "error";
+        ?><script>window.history.back();</script><?php
+    }    
+}catch (Exception $exception){
     session_start();
-    $_SESSION['mensaje'] = "Se elimino los datos de la institución de la manera correcta en la base de datos";
-    $_SESSION['icono'] = "success";
-    header('Location:'.APP_URL."/admin/configuraciones/institucion");
-}else{
-    session_start();
-    $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, comuniquese con el administrador";
-    $_SESSION['icono'] = "error";
-    ?><script>window.history.back();</script><?php
+        $_SESSION['mensaje'] = "Error no se pudo eliminar en la base datos, porque existe este registro en otras tablas";
+        $_SESSION['icono'] = "error";
+        ?><script>window.history.back();</script><?php
 }
 
